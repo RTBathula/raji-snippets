2fc6232d248f3a9003ed2401e5b165a61765a888
teja.batthula <teja.batthula@carnext.com>
2021-04-12 23:54:46 +0200

    added mock rest

---
+++ README.md
@@ -0,0 +1,37 @@
+# Express REST with Mock data
+
+# Getting Started
+Fork and clone the repository. Install dependencies with:
+
+```
+npm install
+```
+
+# Run the server
+After completing the above steps run your node.js server
+
+```
+npm start
+```
+
+This will run the server on port 8081
+
+## API Usage
+
+1. http://localhost:8081/products?skip=0&limit=30&sort=email:desc
+
+Defaults
+```
+skip = 0;
+limit = 30;
+sort = first_name:asc (this will be splited by ':'' to get column and order)
+```
+
+2. http://localhost:8081/cars/year?valid_from=1980
+
+Defaults
+```
+valid_from=1991
+```
+
+
---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+const fs = require('fs');
+
+const isFiniteNumber = value => typeof value === 'number' && !isNaN(value) && isFinite(value);
+
+const sortASC = (valueA, valueB) => {
+	return isFiniteNumber(valueA) ? (valueA - valueB) : valueA.trim().localeCompare(valueB.trim());	
+};
+
+const sortDESC = (valueA, valueB) => {
+	return isFiniteNumber(valueA) ? (valueB - valueA) : valueB.trim().localeCompare(valueA.trim());	
+};
+
+// E.g: /products?skip=0&limit=30&sort=first_name:desc
+app.get('/products', (req, res) => {	
+	let [sortColumn, sortOrder] = (req.query.sort || '').split(':');
+	sortColumn = sortColumn || 'first_name';
+	sortOrder = sortOrder || 'asc';
+
+	const skip = parseInt(req.query.skip || 0);
+	const limit = parseInt(req.query.limit || 20);
+
+
+	fs.readFile('./MOCK_DATA.json', 'utf8', (err, data) => {
+		const list = JSON.parse(data);
+		
+		const sliceLimit = (skip + limit);
+		const finalList = list.slice(skip, sliceLimit).sort((a, b) => {
+			const valueA = a[sortColumn];
+			const valueB = b[sortColumn];
+			return sortOrder === 'asc' ? sortASC(valueA, valueB) : sortDESC(valueA, valueB);					
+		});
+
+	  	return res.status(200).send(finalList);
+	});
+});
+
+// E.g /cars/year?valid_from=1980
+app.get('/cars/year', (req, res) => {
+	const validFrom = parseInt(req.query.valid_from || 1991);
+
+	fs.readFile('./MOCK_DATA_CAR.json', 'utf8', (err, data) => {
+		const list = JSON.parse(data);		
+	
+		const finalList = list.filter((obj) => (obj.car_model_year > validFrom));
+	  	return res.status(200).send(finalList);
+	});
+});
+
+const PORT = 8081;
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
